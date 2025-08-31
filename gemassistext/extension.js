3b5b967b64f5e67bc69f71135166216a27b622f8
elazzi <elazzi@gmail.com>
2025-08-31 18:34:37 +0200

    Update extension.js

--- gemassistext/extension.js
+++ gemassistext/extension.js
@@ -309,7 +309,7 @@ function registerInlineCompletionProvider(context) {
                 return [];
             }
         }
-    ));
+    );
     context.subscriptions.push(inlineCompletionProvider);
 }
 
